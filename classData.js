03e9ba1b9541784a0edb518e45e1001879ebe10a
emerd01 <64674100+emerd01@users.noreply.github.com>
2020-09-23 21:50:21 -0400

    full website with buttons

--- classData.js
+++ classData.js
@@ -6,6 +6,14 @@ var homeworkMean = function (penguin) {
     return d3.mean(homeworkGrade)
 }
 
+var quizMean = function (penguin) {
+    var getQuizGrade = function (quiz) {
+        return quiz.grade
+    }
+    var quizGrade = penguin.quizes.map(getQuizGrade)
+    return d3.mean(quizGrade)
+}
+
 var finalMean = function (penguin) {
     getFinalGrade = function (final) {
         return final.grade
@@ -14,8 +22,7 @@ var finalMean = function (penguin) {
     return d3.mean(finalGrade)
 }
 
-var getPhoto = function(penguin)
-{
+var getPhoto = function (penguin) {
     return "imgs/" + penguin.picture
 }
 
@@ -31,12 +38,12 @@ var drawPlot = function (penguins, screen, xScale, yScale) {
         .attr("cy", function (penguin) {
             return yScale(homeworkMean(penguin))
         })
-        .attr("r",5)
-    
-    
-    
-    
-    
+        .attr("r", 5)
+
+
+
+
+
         .on("mouseenter", function (penguin) {
             console.log("hovering")
             var xPos = d3.event.pageX;
@@ -46,24 +53,157 @@ var drawPlot = function (penguins, screen, xScale, yScale) {
                 .classed("hidden", false)
                 .style("top", yPos + "px")
                 .style("left", xPos + "px")
-        
+
             d3.select("#pics")
-            .append("img")
-            .attr("src", "imgs/" + penguin.picture)
-        
+                .append("img")
+                .attr("src", "imgs/" + penguin.picture)
+
             d3.select("#final")
-            .text(finalMean(penguin))
+                .text(finalMean(penguin))
 
         })
 
-        .on("mouseleave", function(penguin)
-           {
+        .on("mouseleave", function (penguin) {
             d3.select("#tooltip")
-            .classed("hidden", true)
-            
+                .classed("hidden", true)
+
             d3.select("img")
-            .remove()
-    })
+                .remove()
+        })
+
+}
+
+
+var drawHWvQuiz = function (penguins, screen, xScale, yScale) {
+    d3.select("#graph")
+        .selectAll("circle")
+        .data(penguins)
+        .enter()
+        .append("circle")
+        .attr("cx", function (penguin) {
+            return xScale(quizMean(penguin))
+        })
+        .attr("cy", function (penguin) {
+            return yScale(homeworkMean(penguin))
+        })
+        .attr("r", 5)
+
+
+
+
+
+        .on("mouseenter", function (penguin) {
+            console.log("hovering")
+            var xPos = d3.event.pageX;
+            var yPos = d3.event.pageY;
+
+            d3.select("#tooltip")
+                .classed("hidden", false)
+                .style("top", yPos + "px")
+                .style("left", xPos + "px")
+
+            d3.select("#pics")
+                .append("img")
+                .attr("src", "imgs/" + penguin.picture)
+
+        })
+
+        .on("mouseleave", function (penguin) {
+            d3.select("#tooltip")
+                .classed("hidden", true)
+
+            d3.select("img")
+                .remove()
+        })
+
+}
+
+
+var drawFinalvHW = function (penguins, screen, xScale, yScale) {
+    d3.select("#graph")
+        .selectAll("circle")
+        .data(penguins)
+        .enter()
+        .append("circle")
+        .attr("cx", function (penguin) {
+            return xScale(finalMean(penguin))
+        })
+        .attr("cy", function (penguin) {
+            return yScale(homeworkMean(penguin))
+        })
+        .attr("r", 5)
+
+
+
+
+
+        .on("mouseenter", function (penguin) {
+            console.log("hovering")
+            var xPos = d3.event.pageX;
+            var yPos = d3.event.pageY;
+
+            d3.select("#tooltip")
+                .classed("hidden", false)
+                .style("top", yPos + "px")
+                .style("left", xPos + "px")
+
+            d3.select("#pics")
+                .append("img")
+                .attr("src", "imgs/" + penguin.picture)
+
+        })
+
+        .on("mouseleave", function (penguin) {
+            d3.select("#tooltip")
+                .classed("hidden", true)
+
+            d3.select("img")
+                .remove()
+        })
+
+}
+
+var drawFinalvQuiz = function (penguins, screen, xScale, yScale) {
+    d3.select("#graph")
+        .selectAll("circle")
+        .data(penguins)
+        .enter()
+        .append("circle")
+        .attr("cx", function (penguin) {
+            return xScale(finalMean(penguin))
+        })
+        .attr("cy", function (penguin) {
+            return yScale(quizMean(penguin))
+        })
+        .attr("r", 5)
+
+
+
+
+
+        .on("mouseenter", function (penguin) {
+            console.log("hovering")
+            var xPos = d3.event.pageX;
+            var yPos = d3.event.pageY;
+
+            d3.select("#tooltip")
+                .classed("hidden", false)
+                .style("top", yPos + "px")
+                .style("left", xPos + "px")
+
+            d3.select("#pics")
+                .append("img")
+                .attr("src", "imgs/" + penguin.picture)
+
+        })
+
+        .on("mouseleave", function (penguin) {
+            d3.select("#tooltip")
+                .classed("hidden", true)
+
+            d3.select("img")
+                .remove()
+        })
 
 }
 
@@ -88,7 +228,34 @@ var initGraph = function (penguins) {
         .range([screen.height, 0])
 
 
-    drawPlot(penguins, screen, xScale, yScale);
+    drawPlot(penguins, screen, xScale, yScale)
+    
+    d3.select("#hwVquiz")
+        .on("click", function()
+            {
+            d3.selectAll("circle")
+                .remove()
+        drawHWvQuiz(penguins, screen, xScale, yScale)
+    })
+    
+    
+    d3.select("#FinalVhw")
+        .on("click", function()
+            {
+            d3.selectAll("circle")
+                .remove()
+        drawFinalvHW(penguins, screen, xScale, yScale)
+    })
+    
+    d3.select("#FinalvQuiz")
+        .on("click", function()
+            {
+            d3.selectAll("circle")
+                .remove()
+        drawFinalvQuiz(penguins, screen, xScale, yScale)
+    })
+    
+
 
 }
 
